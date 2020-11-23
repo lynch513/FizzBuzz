fb12ee22a334fb7dad5ffbfed00205b7b897b3c0
Sergey M Solomkin <lynch513@yandex.ru>
2020-11-23 18:17:56 +0300

    Naive implementation

--- Program.fs
+++ Program.fs
@@ -1,13 +1,33 @@
-﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+﻿open System
 
-open System
+let getFizzBuzz = function
+    | x when x % 3 = 0 && x % 5 = 0 -> "FizzBuzz"
+    | x when x % 3 = 0 -> "Fizz"
+    | x when x % 5 = 0 -> "Buzz"
+    | x -> string x
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let readIntFromUser () =
+    let x = Console.ReadLine ()  
+    match Int32.TryParse x with
+    | true, d -> Some d
+    | _ -> None
+
+let checkUserInt = function
+    | x when x >= 1 && x <= 4000 -> Some x
+    | _ -> None
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    printfn "Please enter a digit greater than 0 and less or equal than 4000:"
+    let x = readIntFromUser ()  
+    printfn "Output:"
+    match x with
+    | Some x ->
+        match checkUserInt x with
+        | Some x ->
+            [1 .. x]
+            |> Seq.map getFizzBuzz
+            |> Seq.iter (printfn "%s")
+        | _ -> printfn "Enter a digit from 1 to 3999!"
+    | _ -> printfn "Enter a digit!"
     0 // return an integer exit code
