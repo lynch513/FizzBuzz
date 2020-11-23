a8e5cc6071a005016e204956a740a693009211fa
Sergey M Solomkin <lynch513@yandex.ru>
2020-11-23 20:26:28 +0300

    Added Result.fromOption helper. Added Domain and Application modules

--- Program.fs
+++ Program.fs
@@ -22,17 +22,73 @@ module Validate =
         | x when x >= 1 && x <= 4000 -> Some x
         | _ -> None
 
+module Result =
+    let fromOption err = function
+        | Some x -> Ok x
+        | None -> Error err 
+
+module Domain =
+    type ParseNumber = string -> int option
+    type ValidateNumber = int -> int option
+    type GetFizzBuzzNumber = int -> string
+    type GetFizzBuzzSeq = int -> seq<string>
+
+    type ParserError = NotNumber 
+    type ValidationError = InvalidNumber 
+
+    type Error =
+        | ParserError of ParserError
+        | ValidationError of ValidationError
+
+    type ExecuteWorkflow = string -> Result<string, Error>
+
+    let execute 
+        (parseNumber : ParseNumber)
+        (validateNumber : ValidateNumber)
+        (getFizzBuzzSeq: GetFizzBuzzSeq) =
+        let parseNumber x =
+            x
+            |> parseNumber
+            |> Result.fromOption NotNumber
+            |> Result.mapError ParserError
+        let validateNumber x =
+            x
+            |> validateNumber
+            |> Result.fromOption InvalidNumber
+            |> Result.mapError ValidationError
+        fun x ->
+            x
+            |> parseNumber
+            |> Result.bind validateNumber
+            |> Result.map getFizzBuzzSeq
+
+module Application =
+    open Domain
+
+    type Input = unit -> string
+    type Output = string -> unit
+
+    let execute =
+        Domain.execute
+            Parser.tryParse
+            Validate.checkInputRange
+            FizzBuzz.getSeq
+
+    let application (input: Input) (output: Output) =
+        fun () ->
+            output "Please enter a digit greater than 0 and less or equal than 4000:"
+            input ()
+            |> execute
+            |> function
+                | Ok s -> 
+                    sprintf "Output:\n%s" (s |> String.concat "\n")
+                | Error (ParserError NotNumber) -> 
+                    sprintf "Enter a digit!"
+                | Error (ValidationError InvalidNumber) -> 
+                    sprintf "Enter a digit from 1 to 3999!"
+            |> output
+
 [<EntryPoint>]
 let main argv =
-    printfn "Please enter a digit greater than 0 and less or equal than 4000:"
-    let x = Console.ReadLine ()  
-    printfn "Output:"
-    match Parser.tryParse x with
-    | Some x ->
-        match Validate.checkInputRange x with
-        | Some x ->
-            FizzBuzz.getSeq x
-            |> Seq.iter (printfn "%s")
-        | _ -> printfn "Enter a digit from 1 to 3999!"
-    | _ -> printfn "Enter a digit!"
+    Application.application Console.ReadLine Console.WriteLine ()
     0 // return an integer exit code
